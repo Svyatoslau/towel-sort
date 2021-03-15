2ac7fc983ed243bbf150bc6fe346d26e77a8003e
Svyatoslav Yaroshevish <70974206+Svyatoslau@users.noreply.github.com>
2021-03-15 23:38:59 +0300

    fix: array

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort(matrix) {
     if (matrix && matrix.length != 0) {
-        let array = [];
+        var array = [];
         for (let i = 0; i < matrix.length; i++) {
             if (i == 0 || i % 2 == 0) {
                 array = array.contact(matrix[i]);
